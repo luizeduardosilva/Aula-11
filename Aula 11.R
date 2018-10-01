721ca162387c2de1700a06252cc2af52179b0e57
luizeduardosilva <42557096+luizeduardosilva@users.noreply.github.com>
2018-10-01 20:29:43 -0300

    Add files via upload

---
+++ Aula 11.R
@@ -0,0 +1,224 @@
+> install.packages("Hmisc")
+Installing package into 'C:/Users/Alunos/Documents/R/win-library/3.5'
+(as 'lib' is unspecified)
+also installing the dependencies 'evaluate', 'markdown', 'yaml', 'backports', 'knitr', 'checkmate', 'htmlwidgets', 'Formula', 'latticeExtra', 'acepack', 'gridExtra', 'data.table', 'htmlTable', 'viridis', 'base64enc'
+
+
+There are binary versions available but the source versions are later:
+  binary source needs_compilation
+htmlwidgets    1.2    1.3             FALSE
+data.table  1.11.6 1.11.8              TRUE
+
+Binaries will be installed
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/evaluate_0.11.zip'
+Content type 'application/zip' length 73328 bytes (71 KB)
+downloaded 71 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/markdown_0.8.zip'
+Content type 'application/zip' length 189383 bytes (184 KB)
+downloaded 184 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/yaml_2.2.0.zip'
+Content type 'application/zip' length 203553 bytes (198 KB)
+downloaded 198 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/backports_1.1.2.zip'
+Content type 'application/zip' length 54833 bytes (53 KB)
+downloaded 53 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/knitr_1.20.zip'
+Content type 'application/zip' length 1182646 bytes (1.1 MB)
+downloaded 1.1 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/checkmate_1.8.5.zip'
+Content type 'application/zip' length 630675 bytes (615 KB)
+downloaded 615 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/Formula_1.2-3.zip'
+Content type 'application/zip' length 180903 bytes (176 KB)
+downloaded 176 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/latticeExtra_0.6-28.zip'
+Content type 'application/zip' length 2192492 bytes (2.1 MB)
+downloaded 2.1 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/acepack_1.4.1.zip'
+Content type 'application/zip' length 97960 bytes (95 KB)
+downloaded 95 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/gridExtra_2.3.zip'
+Content type 'application/zip' length 1108721 bytes (1.1 MB)
+downloaded 1.1 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/data.table_1.11.6.zip'
+Content type 'application/zip' length 1870008 bytes (1.8 MB)
+downloaded 1.8 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/htmlTable_1.12.zip'
+Content type 'application/zip' length 322576 bytes (315 KB)
+downloaded 315 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/viridis_0.5.1.zip'
+Content type 'application/zip' length 1866694 bytes (1.8 MB)
+downloaded 1.8 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/base64enc_0.1-3.zip'
+Content type 'application/zip' length 43294 bytes (42 KB)
+downloaded 42 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/Hmisc_4.1-1.zip'
+Content type 'application/zip' length 3012758 bytes (2.9 MB)
+downloaded 2.9 MB
+
+package 'evaluate' successfully unpacked and MD5 sums checked
+package 'markdown' successfully unpacked and MD5 sums checked
+package 'yaml' successfully unpacked and MD5 sums checked
+package 'backports' successfully unpacked and MD5 sums checked
+package 'knitr' successfully unpacked and MD5 sums checked
+package 'checkmate' successfully unpacked and MD5 sums checked
+package 'Formula' successfully unpacked and MD5 sums checked
+package 'latticeExtra' successfully unpacked and MD5 sums checked
+package 'acepack' successfully unpacked and MD5 sums checked
+package 'gridExtra' successfully unpacked and MD5 sums checked
+package 'data.table' successfully unpacked and MD5 sums checked
+package 'htmlTable' successfully unpacked and MD5 sums checked
+package 'viridis' successfully unpacked and MD5 sums checked
+package 'base64enc' successfully unpacked and MD5 sums checked
+package 'Hmisc' successfully unpacked and MD5 sums checked
+
+The downloaded binary packages are in
+C:\Users\Alunos\AppData\Local\Temp\RtmpsriEw1\downloaded_packages
+installing the source package 'htmlwidgets'
+
+trying URL 'https://cran.rstudio.com/src/contrib/htmlwidgets_1.3.tar.gz'
+Content type 'application/x-gzip' length 866548 bytes (846 KB)
+downloaded 846 KB
+
+* installing *source* package 'htmlwidgets' ...
+** package 'htmlwidgets' successfully unpacked and MD5 sums checked
+** R
+** inst
+** byte-compile and prepare package for lazy loading
+** help
+*** installing help indices
+converting help for package 'htmlwidgets'
+finding HTML links ... done
+JS                                      html  
+createWidget                            html  
+Rd warning: C:/Users/Alunos/AppData/Local/Temp/RtmpUvhGSr/R.INSTALL22804c956026/htmlwidgets/man/createWidget.Rd:17: file link 'toJSON' in package 'jsonlite' does not exist and so has been treated as a topic
+getDependency                           html  
+htmlwidgets-package                     html  
+htmlwidgets-shiny                       html  
+onRender                                html  
+onStaticRenderComplete                  html  
+Rd warning: C:/Users/Alunos/AppData/Local/Temp/RtmpUvhGSr/R.INSTALL22804c956026/htmlwidgets/man/onStaticRenderComplete.Rd:15: file link 'tags' in package 'htmltools' does not exist and so has been treated as a topic
+prependContent                          html  
+Rd warning: C:/Users/Alunos/AppData/Local/Temp/RtmpUvhGSr/R.INSTALL22804c956026/htmlwidgets/man/prependContent.Rd:15: file link 'tags' in package 'htmltools' does not exist and so has been treated as a topic
+saveWidget                              html  
+scaffoldWidget                          html  
+setWidgetIdSeed                         html  
+sizingPolicy                            html  
+** building package indices
+** installing vignettes
+** testing if installed package can be loaded
+*** arch - i386
+*** arch - x64
+* DONE (htmlwidgets)
+In R CMD INSTALL
+
+The downloaded source packages are in
+'C:\Users\Alunos\AppData\Local\Temp\RtmpsriEw1\downloaded_packages'
+> install.packages("forecast")
+Installing package into 'C:/Users/Alunos/Documents/R/win-library/3.5'
+(as 'lib' is unspecified)
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.5/forecast_8.4.zip'
+Content type 'application/zip' length 1997947 bytes (1.9 MB)
+downloaded 1.9 MB
+
+package 'forecast' successfully unpacked and MD5 sums checked
+
+The downloaded binary packages are in
+C:\Users\Alunos\AppData\Local\Temp\RtmpsriEw1\downloaded_packages
+> library(Hmisc)
+Carregando pacotes exigidos: lattice
+Carregando pacotes exigidos: survival
+Carregando pacotes exigidos: Formula
+Carregando pacotes exigidos: ggplot2
+
+Attaching package: 'Hmisc'
+
+The following objects are masked from 'package:base':
+  
+  format.pval, units
+
+> library(forecast)
+> library(readxl)
+> library(Hmisc)
+> IPCA.df<-read_excel("c:/Econometria/IPCA.xls")
+> plot(IPCA.df$IPCA, type = "l")
+> MM <- data.frame(na.omit(ma(IPCA.df$IPCA,order = 12, centre = T)))
+> a <- (127-nrow(MM))+1
+> IPCA.DF <- as.data.frame(IPCA.df$IPCA[a:127])
+> Tabela1 <- cbind(IPCA.DF,MM)
+> colnames(Tabela1) <- c("IPCA","Média Móvel")
+> View(Tabela1)
+> Grafico <- ts(Tabela1, start = 2008, frequency = 12)
+> plot(Grafico, plot.type= "single", col=c("Black","Blue"))
+> z <- lm(IPCA.df$IPCA~IPCA.df$Ano.Mês)
+Error in model.frame.default(formula = IPCA.df$IPCA ~ IPCA.df$Ano.Mês,  : 
+                               tipo inv�lido (NULL) para vari�vel 'IPCA.df$Ano.Mês'
+                             In addition: Warning message:
+                               Unknown or uninitialised column: 'Ano.Mês'. 
+                             > abline(z, col="Green")
+                             Error in abline(z, col = "Green") : object 'z' not found
+                             > z <- lm(IPCA.df$IPCA~IPCA.df$Ano.Mês)
+                             Error in model.frame.default(formula = IPCA.df$IPCA ~ IPCA.df$Ano.Mês,  : 
+                                                            tipo inv�lido (NULL) para vari�vel 'IPCA.df$Ano.Mês'
+                                                          In addition: Warning message:
+                                                            Unknown or uninitialised column: 'Ano.Mês'. 
+                                                          > z <- lm(IPCA.df$IPCA~IPCA.df$Ano.M�s)
+                                                          > abline(z, col="Green")
+                                                          > summary(z)
+                                                          
+                                                          Call:
+                                                            lm(formula = IPCA.df$IPCA ~ IPCA.df$Ano.M�s)
+                                                          
+                                                          Residuals:
+                                                            Min       1Q   Median       3Q      Max 
+                                                          -0.82458 -0.22349 -0.08363  0.20146  1.19230 
+                                                          
+                                                          Coefficients:
+                                                            Estimate Std. Error t value Pr(>|t|)
+                                                          (Intercept)      7.388e-01  4.464e-01   1.655    0.100
+                                                          IPCA.df$Ano.M�s -2.180e-10  3.262e-10  -0.668    0.505
+                                                          
+                                                          Residual standard error: 0.3545 on 125 degrees of freedom
+                                                          Multiple R-squared:  0.00356,	Adjusted R-squared:  -0.004412 
+                                                          F-statistic: 0.4466 on 1 and 125 DF,  p-value: 0.5052
+                                                          
+                                                          > tabela2 <- as.data.frame(Tabela1$IPCA/Tabela1$`Média Móvel`)
+                                                          > plot(tabela2)
+                                                          > Inflacao <- ts(IPCA.df$IPCA, start = 2008, frequency = 12)
+                                                          > 
+                                                            > decomposicao <- decompose(Inflacao)                                          #Decompõe a série em Ciclo, Tendência e Sazonalidade
+                                                          > 
+                                                            > plot(decompose(Inflacao))  
+                                                          > 
+                                                            > Tendencia <- decomposicao$trend
+                                                          > 
+                                                            > Sazonalidade <- decomposicao$seasonal
+                                                          > 
+                                                            > Ciclo <- decomposicao$random
+                                                          > 
+                                                            > Tab_Dados1 <- data.frame(IPCA.df$IPCA, Ciclo)
+                                                          > 
+                                                            > View(Tab_Dados1)
+                                                          > 
+                                                            > plot(Sazonalidade, type="l")
+                                                          > 
+                                                            > Serie_Tempo1 <- ts(Tab_Dados1, start = 2008, frequency = 12)
+                                                          > plot(Serie_Tempo1, plot.type = "single", col= c("Blue", "Red"))
+                                                          > 
+                                                            > Tab_Dados2 <- data.frame(IPCA.df$IPCA, Tendencia)
+                                                          > Serie_Tempo2 <- ts(Tab_Dados2, start = 2008, frequency = 12)
+                                                          > plot(Serie_Tempo2, plot.type = "single", col= c("Blue", "Red"))
